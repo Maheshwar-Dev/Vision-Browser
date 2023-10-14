00fbb0101f151e9fe51c067ec538ee6870e2798a
Maheshwar-Dev <146438197+Maheshwar-Dev@users.noreply.github.com>
2023-10-14 10:07:50 +0530

    Update themes.py

--- themes.py
+++ themes.py
@@ -1,3 +1,32 @@
+import SystemTheme as systheme
+
 #Colors
 navBar_color = "#FFCC99"  #Must Be In Color Codes Only
-browser_Themes = {"Orange": 0, "Pink": 1, "Lime": 2, "Voilet": 3, "Light": 4, "Dark": 5}
+
+
+browser_Themes = {"Orange":"Orange", "Pink":"Pink", "Lime":"Lime" , "Voilet":"Voilet" , "Cyan":"Cyan", "Light":"Light", "Dark":"Dark", "System":"System"}
+
+
+#Theme Colors Stored In List
+#Components as [MainTheme_ColorCode, MainTheme_RGBCode]
+theme_color_orange =   ["#ff7f27", (255, 127, 39)]
+theme_color_pink =     ["#c429a3", (196, 41, 163)]
+theme_color_lime =     ["#17d456", (23, 212, 86)]
+theme_color_voilet =   ["#9c3fc4", (156, 63, 196)]
+theme_color_cyan =     ["#10e0cf", (16, 224, 207)]
+theme_color_light =    ["#ffffff", (255,255,255)]
+theme_color_dark =     ["#000000", (0,0,0)]
+
+
+
+#Colors For The Background UI
+theme_color_orange_background = ["#f2a068", (242, 160, 104)]
+theme_color_pink_background =   ["#cf80be", (207, 128, 190)]
+theme_color_lime_background =   ["#80e0a0", (128, 224, 160)]
+theme_color_voilet_background = ["#a879bd", (168, 121, 189)]
+theme_color_cyan_background =   ["#93dbd5", (147, 219, 213)]
+theme_color_light_background =  ["#ffffff", (1, 1, 1)]
+theme_color_dark_background =   ["#000000", (0, 0, 0)]
+
+
+
